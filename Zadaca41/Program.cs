cffbae54131ab7db064f4599b616db68e983a6f4
KTsarev99 <kirilltcarev1999@gmail.com>
2022-09-22 02:06:28 +0300

    1

---
+++ Zadaca41/Program.cs
@@ -0,0 +1,13 @@
+﻿Console.WriteLine("введите количеств чисел");
+int m = Convert.ToInt32(Console.ReadLine());
+int []array = new int[m];
+int count = 0;
+for(int i = 0; i<array.Length;i++)
+{
+    array[i]= Convert.ToInt32(Console.ReadLine());
+    if(array[i]>0)
+    {
+        count+=1;
+    }
+}
+Console.WriteLine($"кличество положительных чисел = {count}");
